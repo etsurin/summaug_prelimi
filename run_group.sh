9ad6bc32ef0055e736d54b45bbde1513c2bda8f1
etsurin <59410307+etsurin@users.noreply.github.com>
2023-08-03 01:09:51 +0900

    Add files via upload

--- run_group.sh
+++ run_group.sh
@@ -1,13 +1,13 @@
 for seed in 103 113 123 133 143
 do
-python roberta_main.py --n_sample 180 --epoch 70 --augfile summ_aug --gpu_id 4 --seed $seed
-python roberta_main.py --n_sample 180 --epoch 70 --augfile aeda_aug --gpu_id 4 --seed $seed
-python roberta_main.py --n_sample 180 --epoch 70 --gpu_id 4 --seed $seed
-python roberta_main.py --n_sample 1350 --epoch 18 --augfile summ_aug --gpu_id 4 --seed $seed
-python roberta_main.py --n_sample 1350 --epoch 18 --augfile aeda_aug --gpu_id 4 --seed $seed
-python roberta_main.py --n_sample 1350 --epoch 18 --gpu_id 4 --seed $seed
-python roberta_main.py --augfile summ_aug --gpu_id 4 --seed $seed
-python roberta_main.py --augfile aeda_aug --gpu_id 4 --seed $seed
-python roberta_main.py --gpu_id 4 --seed $seed
+python roberta_main.py --n_sample 180 --epoch 70 --augfile summ_aug --seed $seed
+python roberta_main.py --n_sample 180 --epoch 70 --augfile aeda_aug --seed $seed
+python roberta_main.py --n_sample 180 --epoch 70 --seed $seed
+python roberta_main.py --n_sample 1350 --epoch 18 --augfile summ_aug --seed $seed
+python roberta_main.py --n_sample 1350 --epoch 18 --augfile aeda_aug --seed $seed
+python roberta_main.py --n_sample 1350 --epoch 18 --seed $seed
+python roberta_main.py --augfile summ_aug --seed $seed
+python roberta_main.py --augfile aeda_aug --seed $seed
+python roberta_main.py --seed $seed
 
 done  
